96a27b67e78fffa521d63d81c873f4eee527bb71
viper1745 <georgewu0520@hotmail.com>
2023-06-11 20:24:17 -0400

    finish sprint-2

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,128 +1,137 @@
-const performances=[
-    {DATE:"Mon Sept 06 2021",
-    VENUE:"Ronald Lane",
-    LOCATION:"San Francisco, CA"
-    },
-    {DATE:"Tue Sept 21 2021",
-     VENUE:"Pier 3 East",
-     LOCATION:"San Francisco, CA" 
-    },
-    {DATE:"Fri Oct 15 2021",
-        VENUE:"View Lounge",
-        LOCATION:"San Francisco, CA"
-    },
-    {DATE:"Sat Nov 06 2021",
-        VENUE:"Hyatt Agency",
-        LOCATION:"San Francisco, CA"
-    },
-    {   DATE:"Fri Nov 26 2021",
-        VENUE:"Moscow Center",
-        LOCATION:"San Francisco, CA"
+const performances = [
+    {
+        DATE: "Mon Sept 06 2021",
+        VENUE: "Ronald Lane",
+        LOCATION: "San Francisco, CA"
     },
     {
-        DATE:"Wed Dec 15 2021",
-        VENUE:"Press Club",
-        LOCATION:"San Francisco, CA"
+        DATE: "Tue Sept 21 2021",
+        VENUE: "Pier 3 East",
+        LOCATION: "San Francisco, CA"
+    },
+    {
+        DATE: "Fri Oct 15 2021",
+        VENUE: "View Lounge",
+        LOCATION: "San Francisco, CA"
+    },
+    {
+        DATE: "Sat Nov 06 2021",
+        VENUE: "Hyatt Agency",
+        LOCATION: "San Francisco, CA"
+    },
+    {
+        DATE: "Fri Nov 26 2021",
+        VENUE: "Moscow Center",
+        LOCATION: "San Francisco, CA"
+    },
+    {
+        DATE: "Wed Dec 15 2021",
+        VENUE: "Press Club",
+        LOCATION: "San Francisco, CA"
     }
 
-] 
+]
 // select the parent class
-const parentclasshidden=document.querySelector(".main__title-hidden");
+const parentclasshidden = document.querySelector(".main__title-hidden");
 //create a div for tablet and desktop, doesnot display in mobile;
-const hiddendiv=document.createElement("div");
-hiddendiv.className=("main__div--hidden");
-const hiddendate=document.createElement("p");
-hiddendate.className=("main__div-date--hidden");
-hiddendate.innerText=("DATE");
-const hiddenvenue=document.createElement("p")
-hiddenvenue.className=("main__div-venue--hidden");
-hiddenvenue.innerText=("VENUE");
-const hiddenlocation=document.createElement("p");
-hiddenlocation.className=("main__div-location--hidden");
-hiddenlocation.innerText=("LOCATION");
+const hiddendiv = document.createElement("div");
+hiddendiv.className = ("main__div--hidden");
+//create each p tag
+const hiddendate = document.createElement("p");
+hiddendate.className = ("main__div-date--hidden");
+hiddendate.innerText = ("DATE");
+const hiddenvenue = document.createElement("p")
+hiddenvenue.className = ("main__div-venue--hidden");
+hiddenvenue.innerText = ("VENUE");
+const hiddenlocation = document.createElement("p");
+hiddenlocation.className = ("main__div-location--hidden");
+hiddenlocation.innerText = ("LOCATION");
+//append each p tag to the div
 hiddendiv.appendChild(hiddendate);
 hiddendiv.appendChild(hiddenvenue);
 hiddendiv.appendChild(hiddenlocation);
+//append the div to main
 parentclasshidden.appendChild(hiddendiv);
-performances.forEach((performance)=>{
+//call the function to display the shows
+performances.forEach((performance) => {
     displayshow(performance)
 })
-function displayshow(performance){
-
- 
-// select the parent class
-const parentclass=document.querySelector(".main");
+function displayshow(performance) {
 
 
-//create div for first show
-const showDivClass=document.createElement("div");
-showDivClass.className=("main__show-div");
-// date
-const datedivclass=document.createElement("div");
-datedivclass.className=("main__date-div");
+    // select the parent class
+    const parentclass = document.querySelector(".main");
 
-// create DATE (p) tag
-const Date=document.createElement("p");
-Date.className="main__date";
-Date.innerText="DATE";
 
-const Datedetail=document.createElement("p");
-Datedetail.className="main__date--detail";
-Datedetail.innerText=performance.DATE;
-//append p tag to  date div
-datedivclass.appendChild(Date);
-datedivclass.appendChild(Datedetail);
-//append the date div to show div
-showDivClass.appendChild(datedivclass);
-//append showone div to mainparentclass.appendChild(showDivClass);
-//venue div
-const venuedivclass=document.createElement("div");
-venuedivclass.className=("main__venue-div");
-//venue
-const Venue=document.createElement("p");
-Venue.className="main__venue";
-Venue.innerText="VENUE";
-//venue detail
-const Venuedetail=document.createElement("p");
-Venuedetail.className="main__venue--detail";
-Venuedetail.innerText=performance.VENUE;
-//append p tag to div
-venuedivclass.appendChild(Venue);
-venuedivclass.appendChild(Venuedetail);
-//append venue div to show div
-showDivClass.appendChild(venuedivclass);
+    //create div for first show
+    const showDivClass = document.createElement("div");
+    showDivClass.className = ("main__show-div");
+    // date
+    const datedivclass = document.createElement("div");
+    datedivclass.className = ("main__date-div");
 
-//location div
-const locationdivclass=document.createElement("div");
-locationdivclass.className="main__location-div";
-//location
-const Location=document.createElement("p");
-Location.className=("main__location");
-Location.innerText=("LOCATION");
-//location detail
-const Locationdetail=document.createElement("p");
-Locationdetail.className=("main__location--detail");
-Locationdetail.innerText=(performance.LOCATION)
-//append location to locationdiv
-locationdivclass.appendChild(Location);
-locationdivclass.appendChild(Locationdetail);
-//append to showdiv
-showDivClass.appendChild(locationdivclass);
-//create button div
-const buttonDiv=document.createElement("div");
-buttonDiv.className=("main__button-div")
-//create button
-const button=document.createElement("button");
-button.className=("main__button");
-button.innerText=("BUY TICKETS");   
-//append button to buttondiv
-buttonDiv.appendChild(button);
-showDivClass.appendChild(buttonDiv);
-//append to main
-parentclass.appendChild(showDivClass);
+    // create DATE (p) tag
+    const Date = document.createElement("p");
+    Date.className = "main__date";
+    Date.innerText = "DATE";
+
+    const Datedetail = document.createElement("p");
+    Datedetail.className = "main__date--detail";
+    Datedetail.innerText = performance.DATE;
+    //append p tag to  date div
+    datedivclass.appendChild(Date);
+    datedivclass.appendChild(Datedetail);
+    //append the date div to show div
+    showDivClass.appendChild(datedivclass);
+    //append showone div to mainparentclass.appendChild(showDivClass);
+    //venue div
+    const venuedivclass = document.createElement("div");
+    venuedivclass.className = ("main__venue-div");
+    //venue
+    const Venue = document.createElement("p");
+    Venue.className = "main__venue";
+    Venue.innerText = "VENUE";
+    //venue detail
+    const Venuedetail = document.createElement("p");
+    Venuedetail.className = "main__venue--detail";
+    Venuedetail.innerText = performance.VENUE;
+    //append p tag to div
+    venuedivclass.appendChild(Venue);
+    venuedivclass.appendChild(Venuedetail);
+    //append venue div to show div
+    showDivClass.appendChild(venuedivclass);
+
+    //location div
+    const locationdivclass = document.createElement("div");
+    locationdivclass.className = "main__location-div";
+    //location
+    const Location = document.createElement("p");
+    Location.className = ("main__location");
+    Location.innerText = ("LOCATION");
+    //location detail
+    const Locationdetail = document.createElement("p");
+    Locationdetail.className = ("main__location--detail");
+    Locationdetail.innerText = (performance.LOCATION)
+    //append location to locationdiv
+    locationdivclass.appendChild(Location);
+    locationdivclass.appendChild(Locationdetail);
+    //append to showdiv
+    showDivClass.appendChild(locationdivclass);
+    //create button div
+    const buttonDiv = document.createElement("div");
+    buttonDiv.className = ("main__button-div")
+    //create button
+    const button = document.createElement("button");
+    button.className = ("main__button");
+    button.innerText = ("BUY TICKETS");
+    //append button to buttondiv
+    buttonDiv.appendChild(button);
+    showDivClass.appendChild(buttonDiv);
+    //append to main
+    parentclass.appendChild(showDivClass);
 
 }
 
 
 
-    
+
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -4,19 +4,19 @@ const comments = [
         name: "Connor Walton",
         comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
         date: "02/17/2021",
-        ID:2,
+        ID: 2,
     },
     {
         name: "Emilie Beach",
         comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
         date: "01/09/2021",
-        ID:1
+        ID: 1
     },
     {
         name: "Miles Acosta",
         comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
         date: "12/20/2020",
-        ID:0
+        ID: 0
     }
 ]
 
@@ -24,36 +24,41 @@ const comments = [
 //select form
 const form = document.querySelector("form");
 //select parent class
-const Parentclass=document.querySelector(".comment-list");
+const Parentclass = document.querySelector(".comment-list");
 //clear comments before printing out all the comment
-comments.forEach((comment)=>{
+comments.forEach((comment) => {
     displaycomments(comment);
-    });
-    //function to add date to comment
-    function gettodaydate() {
-        let today = new Date();
-        let tomorrow = new Date();
-        
-        let date = String(today.getDate());
-        let month = String((today.getMonth()) + 1);
-    
-        let year = String(today.getFullYear());
-        let dayzero = "";
-        let monthzero = "";
-        if (Number(date) < 10) {
-            dayzero = "0";
-        }
-        if (Number(date) < 10 && Number(month) < 10) {
-            dayzero = "0";
-            monthzero = "0";
-        }
-        if (Number(month) < 10) {
-            monthzero = "0";
-        }
-        today = `${dayzero}${date}/${monthzero}${month}/${year}`
-        return today;
-    }
+    resetplace();   
+});
+//function to add date to comment
+function gettodaydate() {
+    let today = new Date();
+    let tomorrow = new Date();
 
+    let date = String(today.getDate());
+    let month = String((today.getMonth()) + 1);
+
+    let year = String(today.getFullYear());
+    let dayzero = "";
+    let monthzero = "";
+    if (Number(date) < 10) {
+        dayzero = "0";
+    }
+    if (Number(date) < 10 && Number(month) < 10) {
+        dayzero = "0";
+        monthzero = "0";
+    }
+    if (Number(month) < 10) {
+        monthzero = "0";
+    }
+    today = `${dayzero}${date}/${monthzero}${month}/${year}`
+    return today;
+}
+//reset place holder to default
+function resetplace(){
+    document.getElementById("Name").placeholder="Name";
+    document.getElementById("Comment").placeholder="Comment";
+}
 //add event listener submit
 form.addEventListener("submit", (event) => {
     //prevent default
@@ -63,76 +68,73 @@ form.addEventListener("submit", (event) => {
     //get name and comment
     const name = form.Name.value;
     const comment = form.Comment.value;
-    
-    
+
+
     //add new comment to comment array
     const newcommentobject = {
         name: name,
         comment: comment,
         date: gettodaydate(),
-        ID:(comments.length)
+        ID: (comments.length)
     }
     //sort the comment by time order
-Parentclass.innerHTML = "";
+    Parentclass.innerHTML = "";
     comments.push(newcommentobject);
     const sortedcomments = comments.sort((a, b) => {
-        return b.ID-a.ID;
+        return b.ID - a.ID;
     })
     //display all the comment in the array
-    comments.forEach((comment)=>{
+    comments.forEach((comment) => {
         displaycomments(comment);
-        });
+    });
+    //reset placeholder to empty
+    form.Name.value="";
+    form.Comment.value="";
+    resetplace();
     console.log(comments)
 })
 
 //use for each to display each objects
-
 //use DOM to append the objects
-
-function displaycomments(comment){
-//locate parent 
-
-
-//clear out existing comments to add new ones
-
-//create div for the whole comment
-const Comment=document.createElement("div");
-Comment.className=("comment");
-//create div for comment context
-const commentContent=document.createElement("div");
-commentContent.className=("comment__content");
-//create the img and add the img
-const commentImg=document.createElement("img");
-commentImg.src="./assets/Images/Mohan-muruge.jpg";
-commentImg.classList.add('comment__image')
-//create title box
-const commentTitle=document.createElement("div");
-commentTitle.className=("comment__title");
-//create and fill text for username
-const commentName=document.createElement("p");
-commentName.className=("comment__username");
-commentName.innerText=(comment.name);
-//create and fill text for date
-const commentDate=document.createElement("p");
-commentDate.className=("comment__date");
-commentDate.innerText=(comment.date);
-//create and fill text for the text
-const commentText=document.createElement("p");
-commentText.className=("comment__text");
-commentText.innerText=(comment.comment);
-//append date and username to div title
-commentTitle.appendChild(commentName);
-commentTitle.appendChild(commentDate);
-//append text to content
-commentContent.appendChild(commentTitle);
-//append title to content
-commentContent.appendChild(commentText);
-//APPEND img to comment
-Comment.appendChild(commentImg);
-//append content to comment
-Comment.appendChild(commentContent);
-//append comment to parten
-Parentclass.appendChild(Comment);
+function displaycomments(comment) {
+    //create div for the whole comment
+    const Comment = document.createElement("div");
+    Comment.className = ("comment");
+    //create div for comment context
+    const commentContent = document.createElement("div");
+    commentContent.className = ("comment__content");
+    //create the img and add the img
+    const commentImg = document.createElement("img");
+    commentImg.src = "./assets/Images/Mohan-muruge.jpg";
+    commentImg.classList.add('comment__image')
+    //create title box
+    const commentTitle = document.createElement("div");
+    commentTitle.className = ("comment__title");
+    //create and fill text for username
+    const commentName = document.createElement("p");
+    commentName.className = ("comment__username");
+    commentName.innerText = (comment.name);
+    //create and fill text for date
+    const commentDate = document.createElement("p");
+    commentDate.className = ("comment__date");
+    commentDate.innerText = (comment.date);
+    //create and fill text for the text
+    const commentText = document.createElement("p");
+    commentText.className = ("comment__text");
+    commentText.innerText = (comment.comment);
+    //append date and username to div title
+    commentTitle.appendChild(commentName);
+    commentTitle.appendChild(commentDate);
+    //append text to content
+    commentContent.appendChild(commentTitle);
+    //append title to content
+    commentContent.appendChild(commentText);
+    //APPEND img to comment
+    Comment.appendChild(commentImg);
+    //append content to comment
+    Comment.appendChild(commentContent);
+    //append comment to parten
+    Parentclass.appendChild(Comment);
 
 
 }
