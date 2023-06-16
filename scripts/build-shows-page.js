e686c69df95df714a44f06434a54849a85dd3829
viper1745 <georgewu0520@hotmail.com>
2023-06-16 17:03:37 -0400

    s

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,137 +1,178 @@
-const performances = [
-    {
-        DATE: "Mon Sept 06 2021",
-        VENUE: "Ronald Lane",
-        LOCATION: "San Francisco, CA"
-    },
-    {
-        DATE: "Tue Sept 21 2021",
-        VENUE: "Pier 3 East",
-        LOCATION: "San Francisco, CA"
-    },
-    {
-        DATE: "Fri Oct 15 2021",
-        VENUE: "View Lounge",
-        LOCATION: "San Francisco, CA"
-    },
-    {
-        DATE: "Sat Nov 06 2021",
-        VENUE: "Hyatt Agency",
-        LOCATION: "San Francisco, CA"
-    },
-    {
-        DATE: "Fri Nov 26 2021",
-        VENUE: "Moscow Center",
-        LOCATION: "San Francisco, CA"
-    },
-    {
-        DATE: "Wed Dec 15 2021",
-        VENUE: "Press Club",
-        LOCATION: "San Francisco, CA"
-    }
+const baseUrl = "https://project-1-api.herokuapp.com/";
+const apiKey = "d9d7bc3f-4bdb-4e5f-bf65-1a3f4e8ec716 ";
+//get API key only used once
+// function fetchKey() {
+//     // prettier-ignore
+//     axios
+//       .get(`${baseUrl}register`)
+//       .then(
+//         (response) => {
+//           console.log(response);
+//         }
+//       )
+//       .catch(
+//         (error) => {
+//           console.error(error);
+//         }
+//       );
+//   }
+// fetchKey();
+// const performances = [
+//     {
+//         DATE: "Mon Sept 06 2021",
+//         VENUE: "Ronald Lane",
+//         LOCATION: "San Francisco, CA"
+//     },
+//     {
+//         DATE: "Tue Sept 21 2021",
+//         VENUE: "Pier 3 East",
+//         LOCATION: "San Francisco, CA"
+//     },
+//     {
+//         DATE: "Fri Oct 15 2021",
+//         VENUE: "View Lounge",
+//         LOCATION: "San Francisco, CA"
+//     },
+//     {
+//         DATE: "Sat Nov 06 2021",
+//         VENUE: "Hyatt Agency",
+//         LOCATION: "San Francisco, CA"
+//     },
+//     {
+//         DATE: "Fri Nov 26 2021",
+//         VENUE: "Moscow Center",
+//         LOCATION: "San Francisco, CA"
+//     },
+//     {
+//         DATE: "Wed Dec 15 2021",
+//         VENUE: "Press Club",
+//         LOCATION: "San Francisco, CA"
+//     }
 
-]
+// ]
+function getshow() {
+  axios
+    .get(`${baseUrl}showdates?api_key=${apiKey}`)
+    .then((response) => {
+      console.log(response);
+      const performances = response.data;
+      performances.forEach((performance) => {
+        displayshow(performance);
+      });
+    })
+
+    .catch((error) => {
+      console.error(error);
+    });
+}
 // select the parent class
 const parentclasshidden = document.querySelector(".main__title-hidden");
 //create a div for tablet and desktop, doesnot display in mobile;
 const hiddendiv = document.createElement("div");
-hiddendiv.className = ("main__div--hidden");
+hiddendiv.className = "main__heading";
 //create each p tag
 const hiddendate = document.createElement("p");
-hiddendate.className = ("main__div-date--hidden");
-hiddendate.innerText = ("DATE");
-const hiddenvenue = document.createElement("p")
-hiddenvenue.className = ("main__div-venue--hidden");
-hiddenvenue.innerText = ("VENUE");
+hiddendate.className = "heading__text";
+hiddendate.innerText = "DATE";
+const hiddenvenue = document.createElement("p");
+hiddenvenue.className = "heading__text";
+hiddenvenue.innerText = "VENUE";
 const hiddenlocation = document.createElement("p");
-hiddenlocation.className = ("main__div-location--hidden");
-hiddenlocation.innerText = ("LOCATION");
+hiddenlocation.className = "heading__text";
+hiddenlocation.innerText = "LOCATION";
 //append each p tag to the div
+const hiddenPlaceholder = document.createElement("p");
+hiddenPlaceholder.className = "heading__text"
 hiddendiv.appendChild(hiddendate);
 hiddendiv.appendChild(hiddenvenue);
 hiddendiv.appendChild(hiddenlocation);
+hiddendiv.appendChild(hiddenPlaceholder)
 //append the div to main
 parentclasshidden.appendChild(hiddendiv);
 //call the function to display the shows
-performances.forEach((performance) => {
-    displayshow(performance)
-})
+getshow();
 function displayshow(performance) {
+  // select the parent class
+  const parentclass = document.querySelector(".main");
+
+  //create div for first show
+  const showDivClass = document.createElement("div");
+  showDivClass.className = "main__show-div";
+
+  showDivClass.addEventListener('click', () => {
+
+    document.querySelectorAll('.main__show-div').forEach(el => {
+      el.classList.remove('clicked')
+    })
 
 
-    // select the parent class
-    const parentclass = document.querySelector(".main");
+    showDivClass.classList.add('clicked')
+
+  })
 
 
-    //create div for first show
-    const showDivClass = document.createElement("div");
-    showDivClass.className = ("main__show-div");
-    // date
-    const datedivclass = document.createElement("div");
-    datedivclass.className = ("main__date-div");
+  // date
+  const datedivclass = document.createElement("div");
+  datedivclass.className = "main__date-div";
 
-    // create DATE (p) tag
-    const Date = document.createElement("p");
-    Date.className = "main__date";
-    Date.innerText = "DATE";
+  // create DATE (p) tag
+  const Date = document.createElement("p");
+  Date.className = "main__date";
+  Date.innerText = "DATE";
 
-    const Datedetail = document.createElement("p");
-    Datedetail.className = "main__date--detail";
-    Datedetail.innerText = performance.DATE;
-    //append p tag to  date div
-    datedivclass.appendChild(Date);
-    datedivclass.appendChild(Datedetail);
-    //append the date div to show div
-    showDivClass.appendChild(datedivclass);
-    //append showone div to mainparentclass.appendChild(showDivClass);
-    //venue div
-    const venuedivclass = document.createElement("div");
-    venuedivclass.className = ("main__venue-div");
-    //venue
-    const Venue = document.createElement("p");
-    Venue.className = "main__venue";
-    Venue.innerText = "VENUE";
-    //venue detail
-    const Venuedetail = document.createElement("p");
-    Venuedetail.className = "main__venue--detail";
-    Venuedetail.innerText = performance.VENUE;
-    //append p tag to div
-    venuedivclass.appendChild(Venue);
-    venuedivclass.appendChild(Venuedetail);
-    //append venue div to show div
-    showDivClass.appendChild(venuedivclass);
+  const Datedetail = document.createElement("p");
+  Datedetail.className = "main__date--detail";
+  Datedetail.innerText = performance.DATE;
+  //append p tag to  date div
+  datedivclass.appendChild(Date);
+  datedivclass.appendChild(Datedetail);
+  //append the date div to show div
+  showDivClass.appendChild(datedivclass);
+  //append showone div to mainparentclass.appendChild(showDivClass);
+  //venue div
+  const venuedivclass = document.createElement("div");
+  venuedivclass.className = "main__venue-div";
+  //venue
+  const Venue = document.createElement("p");
+  Venue.className = "main__venue";
+  Venue.innerText = "VENUE";
+  //venue detail
+  const Venuedetail = document.createElement("p");
+  Venuedetail.className = "main__venue--detail";
+  Venuedetail.innerText = performance.VENUE;
+  //append p tag to div
+  venuedivclass.appendChild(Venue);
+  venuedivclass.appendChild(Venuedetail);
+  //append venue div to show div
+  showDivClass.appendChild(venuedivclass);
 
-    //location div
-    const locationdivclass = document.createElement("div");
-    locationdivclass.className = "main__location-div";
-    //location
-    const Location = document.createElement("p");
-    Location.className = ("main__location");
-    Location.innerText = ("LOCATION");
-    //location detail
-    const Locationdetail = document.createElement("p");
-    Locationdetail.className = ("main__location--detail");
-    Locationdetail.innerText = (performance.LOCATION)
-    //append location to locationdiv
-    locationdivclass.appendChild(Location);
-    locationdivclass.appendChild(Locationdetail);
-    //append to showdiv
-    showDivClass.appendChild(locationdivclass);
-    //create button div
-    const buttonDiv = document.createElement("div");
-    buttonDiv.className = ("main__button-div")
-    //create button
-    const button = document.createElement("button");
-    button.className = ("main__button");
-    button.innerText = ("BUY TICKETS");
-    //append button to buttondiv
-    buttonDiv.appendChild(button);
-    showDivClass.appendChild(buttonDiv);
-    //append to main
-    parentclass.appendChild(showDivClass);
-
+  //location div
+  const locationdivclass = document.createElement("div");
+  locationdivclass.className = "main__location-div";
+  //location
+  const Location = document.createElement("p");
+  Location.className = "main__location";
+  Location.innerText = "LOCATION";
+  //location detail
+  const Locationdetail = document.createElement("p");
+  Locationdetail.className = "main__location--detail";
+  Locationdetail.innerText = performance.LOCATION;
+  //append location to locationdiv
+  locationdivclass.appendChild(Location);
+  locationdivclass.appendChild(Locationdetail);
+  //append to showdiv
+  showDivClass.appendChild(locationdivclass);
+  //create button div
+  const buttonDiv = document.createElement("div");
+  buttonDiv.className = "main__button-div";
+  //create button
+  const button = document.createElement("button");
+  button.className = "main__button";
+  button.innerText = "BUY TICKETS";
+  //append button to buttondiv
+  buttonDiv.appendChild(button);
+  showDivClass.appendChild(buttonDiv);
+  //append to main
+  parentclass.appendChild(showDivClass);
 }
-
-
-
 
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,144 +1,178 @@
+const baseUrl = "https://project-1-api.herokuapp.com/";
+const apiKey = "d9d7bc3f-4bdb-4e5f-bf65-1a3f4e8ec716";
+function getComment() {
+  axios
+    .get(`${baseUrl}comments?api_key=${apiKey}`)
+    .then((response) => {
+      console.log(response);
+      const comments = response.data;
+      comments.sort((a, b) => {
+        return b.timestamp - a.timestamp;
+      });
+      console.log(comments);
+      comments.forEach((comment) => {
+        comment.timestamp = gettodaydate(comment.timestamp);
+        displaycomments(comment);
+        resetplace();
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+getComment();
+function postCommet(name, comment) {
+  axios
+    .post(`${baseUrl}comments?api_key=${apiKey}`, {
+      name: name,
+      comment: comment,
+    })
+    .then((response) => {
+      getComment();
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
 //object list
-const comments = [
-    {
-        name: "Connor Walton",
-        comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-        date: "02/17/2021",
-        ID: 2,
-    },
-    {
-        name: "Emilie Beach",
-        comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-        date: "01/09/2021",
-        ID: 1
-    },
-    {
-        name: "Miles Acosta",
-        comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-        date: "12/20/2020",
-        ID: 0
-    }
-]
-
+// const comments = [
+//     {
+//         name: "Connor Walton",
+//         comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+//         date: "02/17/2021",
+//         ID: 2,
+//     },
+//     {
+//         name: "Emilie Beach",
+//         comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+//         date: "01/09/2021",
+//         ID: 1
+//     },
+//     {
+//         name: "Miles Acosta",
+//         comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+//         date: "12/20/2020",
+//         ID: 0
+//     }
+// ]
 
 //select form
 const form = document.querySelector("form");
 //select parent class
 const Parentclass = document.querySelector(".comment-list");
 //clear comments before printing out all the comment
-comments.forEach((comment) => {
-    displaycomments(comment);
-    resetplace();   
-});
+// comments.forEach((comment) => {
+//     displaycomments(comment);
+//     resetplace();
+// });
 //function to add date to comment
-function gettodaydate() {
-    let today = new Date();
-    let tomorrow = new Date();
+function gettodaydate(today) {
+  exactdate = new Date(today);
 
-    let date = String(today.getDate());
-    let month = String((today.getMonth()) + 1);
+  let date = String(exactdate.getDate());
+  let month = String(exactdate.getMonth() + 1);
 
-    let year = String(today.getFullYear());
-    let dayzero = "";
-    let monthzero = "";
-    if (Number(date) < 10) {
-        dayzero = "0";
-    }
-    if (Number(date) < 10 && Number(month) < 10) {
-        dayzero = "0";
-        monthzero = "0";
-    }
-    if (Number(month) < 10) {
-        monthzero = "0";
-    }
-    today = `${dayzero}${date}/${monthzero}${month}/${year}`
-    return today;
+  let year = String(exactdate.getFullYear());
+  let dayzero = "";
+  let monthzero = "";
+  if (Number(date) < 10) {
+    dayzero = "0";
+  }
+  if (Number(date) < 10 && Number(month) < 10) {
+    dayzero = "0";
+    monthzero = "0";
+  }
+  if (Number(month) < 10) {
+    monthzero = "0";
+  }
+  exactdate = `${dayzero}${date}/${monthzero}${month}/${year}`;
+
+  return exactdate;
 }
 //reset place holder to default
-function resetplace(){
-    document.getElementById("Name").placeholder="Name";
-    document.getElementById("Comment").placeholder="Comment";
+function resetplace() {
+  document.getElementById("Name").placeholder = "Enter your Name";
+  document.getElementById("Comment").placeholder = "Add a new comment";
 }
 //add event listener submit
 form.addEventListener("submit", (event) => {
-    //prevent default
-    event.preventDefault();
-    //target event
-    const form = event.target;
-    //get name and comment
-    const name = form.Name.value;
-    const comment = form.Comment.value;
+  //prevent default
+  event.preventDefault();
+  //target event
+  const form = event.target;
+  //get name and comment
+  const name = form.Name.value;
+  const comment = form.Comment.value;
 
-
-    //add new comment to comment array
-    const newcommentobject = {
-        name: name,
-        comment: comment,
-        date: gettodaydate(),
-        ID: (comments.length)
-    }
-    //sort the comment by time order
-    Parentclass.innerHTML = "";
-    comments.push(newcommentobject);
-    const sortedcomments = comments.sort((a, b) => {
-        return b.ID - a.ID;
-    })
-    //display all the comment in the array
-    comments.forEach((comment) => {
-        displaycomments(comment);
-    });
-    //reset placeholder to empty
-    form.Name.value="";
-    form.Comment.value="";
-    resetplace();
-    console.log(comments)
-})
+  //add new comment to comment array
+  // const newcommentobject = {
+  //     name: name,
+  //     comment: comment,
+  //     date: gettodaydate(),
+  //     ID: (comments.length)
+  // }
+  //sort the comment by time order
+ 
+  // comments.push(newcommentobject);
+  // const sortedcomments = comments.sort((a, b) => {
+  //     return b.ID - a.ID;
+  // })
+  //display all the comment in the array
+  // comments.forEach((comment) => {
+  //     displaycomments(comment);
+  // });
+  //reset placeholder to empty
+  if (name === "" && comment === "") {
+    alert("Please enter a name and comment!");
+    return;
+  }
+  Parentclass.innerHTML = "";
+  postCommet(name, comment);
+  form.Name.value = "";
+  form.Comment.value = "";
+  resetplace();
+  // console.log(comments)
+});
 
 //use for each to display each objects
 //use DOM to append the objects
 function displaycomments(comment) {
-    //create div for the whole comment
-    const Comment = document.createElement("div");
-    Comment.className = ("comment");
-    //create div for comment context
-    const commentContent = document.createElement("div");
-    commentContent.className = ("comment__content");
-    //create the img and add the img
-    const commentImg = document.createElement("img");
-    commentImg.src = "./assets/Images/Mohan-muruge.jpg";
-    commentImg.classList.add('comment__image')
-    //create title box
-    const commentTitle = document.createElement("div");
-    commentTitle.className = ("comment__title");
-    //create and fill text for username
-    const commentName = document.createElement("p");
-    commentName.className = ("comment__username");
-    commentName.innerText = (comment.name);
-    //create and fill text for date
-    const commentDate = document.createElement("p");
-    commentDate.className = ("comment__date");
-    commentDate.innerText = (comment.date);
-    //create and fill text for the text
-    const commentText = document.createElement("p");
-    commentText.className = ("comment__text");
-    commentText.innerText = (comment.comment);
-    //append date and username to div title
-    commentTitle.appendChild(commentName);
-    commentTitle.appendChild(commentDate);
-    //append text to content
-    commentContent.appendChild(commentTitle);
-    //append title to content
-    commentContent.appendChild(commentText);
-    //APPEND img to comment
-    Comment.appendChild(commentImg);
-    //append content to comment
-    Comment.appendChild(commentContent);
-    //append comment to parten
-    Parentclass.appendChild(Comment);
-
-
+  //create div for the whole comment
+  const Comment = document.createElement("div");
+  Comment.className = "comment";
+  //create div for comment context
+  const commentContent = document.createElement("div");
+  commentContent.className = "comment__content";
+  //create the img and add the img
+  const commentImg = document.createElement("img");
+  commentImg.src = "./assets/Images/Mohan-muruge.jpg";
+  commentImg.classList.add("comment__image");
+  //create title box
+  const commentTitle = document.createElement("div");
+  commentTitle.className = "comment__title";
+  //create and fill text for username
+  const commentName = document.createElement("p");
+  commentName.className = "comment__username";
+  commentName.innerText = comment.name;
+  //create and fill text for date
+  const commentDate = document.createElement("p");
+  commentDate.className = "comment__date";
+  commentDate.innerText = comment.timestamp;
+  //create and fill text for the text
+  const commentText = document.createElement("p");
+  commentText.className = "comment__text";
+  commentText.innerText = comment.comment;
+  //append date and username to div title
+  commentTitle.appendChild(commentName);
+  commentTitle.appendChild(commentDate);
+  //append text to content
+  commentContent.appendChild(commentTitle);
+  //append title to content
+  commentContent.appendChild(commentText);
+  //APPEND img to comment
+  Comment.appendChild(commentImg);
+  //append content to comment
+  Comment.appendChild(commentContent);
+  //append comment to parten
+  Parentclass.appendChild(Comment);
 }
-
-
-
-
